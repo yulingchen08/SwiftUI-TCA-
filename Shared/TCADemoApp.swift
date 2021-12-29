b56f6a55b6b6aaa5fcc43624c49057489ec4b56c
Yuling <yulingchen08@gmail.com>
2021-12-29 11:29:53 +0800

    Initial Commit

---
+++ Shared/TCADemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADemoApp.swift
+//  Shared
+//
+//  Created by 陳鈺翎 on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct TCADemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
